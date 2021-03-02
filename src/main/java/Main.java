4188d26432d78b0420a52329d4404f44e6c4b942
Azzadal <andr280389@yandex.ru>
2021-03-02 11:23:56 +0700

    wip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,59 @@
-public class Main {
+import org.lwjgl.glfw.GLFWVidMode;
+
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Main implements Runnable {
+    private Thread thread;
+    private boolean running = true;
+    private long window;
+
     public static void main(String[] args) {
-        try {
-            boolean vSync = true;
-            IGameLogic gameLogic = new DummyGame();
-            GameEngine gameEng = new GameEngine("GAME",
-                    600, 480, vSync, gameLogic);
-            gameEng.start();
-        } catch (Exception excp) {
-            excp.printStackTrace();
-            System.exit(-1);
+        Main game  = new Main();
+        game.start();
+    }
+
+    public void start(){
+        running = true;
+        thread = new Thread(this, "Test app");
+        thread.start();
+    }
+
+    public void init(){
+        if (!glfwInit()) System.err.println("Failed");
+
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+        window = glfwCreateWindow(800, 600, "My App", NULL, NULL);
+
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+        glfwSetWindowPos(window, 100, 100);
+
+        glfwMakeContextCurrent(window);
+
+        glfwShowWindow(window);
+    }
+
+    public void update(){
+        glfwPollEvents();
+    }
+
+    public void render(){
+        glfwSwapBuffers(window);
+    }
+
+    @Override
+    public void run() {
+        init();
+        while (running){
+            update();
+            render();
+
+            if (glfwWindowShouldClose(window)) running = false;
         }
     }
 }
